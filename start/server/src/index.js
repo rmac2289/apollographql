4774c4788c5ac5bcbf4307b9a860627991f16af3
Ross MacDonald <rmac2289@gmail.com>
2020-11-25 02:01:30 -0800

    apollographql practice

---
+++ start/server/src/index.js
@@ -0,0 +1,26 @@
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const resolvers = require("./resolvers");
+const { createStore } = require("./utils");
+
+const LaunchAPI = require("./datasources/launch");
+const UserAPI = require("./datasources/user");
+
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store }),
+  }),
+});
+
+server.listen().then(() => {
+  console.log(`
+    Server is running!
+    Listening on port 4000
+    Explore at https://studio.apollographql.com/dev
+  `);
+});
